c688bd89a148d5361657824ee95a7feeacea0602
melaniekung <71947221+melaniekung@users.noreply.github.com>
2020-10-27 15:16:07 -0700

    Revert "Login feature mku 03"

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user (USERNAME,EMAIL,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN) VALUES('admin','admin@gmail.com','adminpass','adminpass',TRUE);
-INSERT INTO user (USERNAME,EMAIL,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN) VALUES('melanie','melanie@gmail.com','melaniepass','melaniepass',FALSE);
-INSERT INTO user (USERNAME,EMAIL,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN) VALUES('rita','rita@gmail.com','ritapass','ritapass',FALSE);
-INSERT INTO user (USERNAME,EMAIL,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN) VALUES('tarek','melanie@gmail.com','tarekpass','tarekpass',FALSE);
