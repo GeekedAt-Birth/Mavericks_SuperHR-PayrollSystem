86f83a968cb8baf159a70a1c8d6ee2d1ef0c8ec9
Rita_Siima <54131784+GeekedAt-Birth@users.noreply.github.com>
2020-11-10 17:30:31 -0800

    Revert "added the Pay_mavericks Entity"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,3 @@ INSERT INTO USERS_MAVERICKS (USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PHONE_NUMBER,PA
 INSERT INTO USERS_MAVERICKS (USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PHONE_NUMBER,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN,JOB_ID) VALUES('melanie','melanie@gmail.com','Melanie','kung','123123123','melaniepass','melaniepass',False,2);
 INSERT INTO USERS_MAVERICKS (USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PHONE_NUMBER,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN,JOB_ID) VALUES('rita','rita@gmail.com','Rita','Ak','123123123','ritapass','ritapass',FALSE,2);
 INSERT INTO USERS_MAVERICKS (USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PHONE_NUMBER,PASSWORD,CONFIRM_PASSWORD,IS_ADMIN,JOB_ID) VALUES('tarek','tarek@gmail.com','Tarek','Ahmed','123123123','tarekpass','tarekpass',FALSE,2);
-
-INSERT INTO PAY_MAVERICKS (benefitEI, benefitCPP, income_Tax, sick_Days, holidays, start_Date, end_Date, employeeId) VALUES (0.0158, 0.0525, 0.15, 14, 10, '2020-10-28 03:36:48', '2020-11-09 13:16:24', 1);
-INSERT INTO PAY_MAVERICKS (benefitEI, benefitCPP, income_Tax, sick_Days, holidays, start_Date, end_Date, employeeId) VALUES (0.0158, 0.0525, 0.15, 14, 15, '2020-10-28 07:54:58', '2020-11-07 11:41:22', 2);
-INSERT INTO PAY_MAVERICKS (benefitEI, benefitCPP, income_Tax, sick_Days, holidays, start_Date, end_Date, employeeId) VALUES (0.0158, 0.0525, 0.15, 14, 15, '2020-10-19 16:12:55', '2020-11-12 15:09:08', 3);
-INSERT INTO PAY_MAVERICKS (benefitEI, benefitCPP, income_Tax, sick_Days, holidays, start_Date, end_Date, employeeId) VALUES (0.0158, 0.0525, 0.15, 7, 10, '2020-10-23 09:16:39', '2020-11-12 23:37:16', 4);
