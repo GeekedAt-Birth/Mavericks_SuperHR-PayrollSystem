2e0f5d7bedb7386f4639cc373eccb3bd0fe72991
melaniekung <kungm1@student.douglascollege.ca>
2020-11-19 22:44:47 -0800

    Added dateBinder() to EmployerProfileController_Mavericks, updated code to allow admins to update start and end dates for pay entity.

--- src/main/java/com/csis3275/controller/EmployerProfileController_Mavericks.java
+++ src/main/java/com/csis3275/controller/EmployerProfileController_Mavericks.java
@@ -1,10 +1,15 @@
 package com.csis3275.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,53 +24,66 @@ import com.csis3275.model.Users_Mavericks;
 
 @Controller
 public class EmployerProfileController_Mavericks {
-	
+
 	@Autowired
 	UserRepository_Mavericks userRepo;
-	
+
 	@Autowired
 	JobsRepository_Mavericks jobRepo;
-	
+
 	@Autowired
 	PayrollDao_Mavericks payRepo;
-	
+
 	@RequestMapping("/admin_mavericks")
 	public String showEmployees(Model model) {
 		List<Users_Mavericks> users = userRepo.findAllEmployees();
-		
+
 		model.addAttribute("users", users);
-		
+
 		return "admin_mavericks";
 	}
-	
+
 	@RequestMapping("/employee")
 	public String showEmployeeInformation(@RequestParam int userId, Model model) {
-		
+
 		Users_Mavericks updUser = userRepo.findById(userId);
 		Jobs_Mavericks job = jobRepo.findById(updUser.getJobId());
 		Pay_Mavericks pay = payRepo.findByEmployeeID(userId);
-		
+
 		model.addAttribute("user", updUser);
 		model.addAttribute("job", job);
 		model.addAttribute("pay", pay);
-		
+
 		return "employee_mavericks";
 	}
-	
-	@PostMapping("/updateEmployeeInfo")
-	public ModelAndView updateEmployeeInfo(Users_Mavericks updUser, Pay_Mavericks pay, Model model) {
-		
-		ModelAndView admin = new ModelAndView("redirect:/admin_mavericks");	
-		
-		Pay_Mavericks updPay = payRepo.findByEmployeeID(updUser.getId());
-		updPay.setSickDays(pay.getSickDays());
-		updPay.setHolidays(pay.getHolidays());
-		
-		payRepo.save(updPay);
-		
-		model.addAttribute("message", "Updated Employee ID " + updUser.getId());
-		
-		return admin;
+
+	/**
+	 * This is to format the date format from database and hmtl input
+	 * 
+	 * @param binder
+	 */
+	@InitBinder
+	private void dateBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		CustomDateEditor editor = new CustomDateEditor(dateFormat, true);
+		binder.registerCustomEditor(Date.class, editor);
 	}
 
+	@PostMapping("/updateEmployeeInfo")
+	public ModelAndView updateEmployeeInfo(Users_Mavericks updUser, Pay_Mavericks pay, Model model) {
+
+		ModelAndView admin = new ModelAndView("redirect:/admin_mavericks");
+
+		Pay_Mavericks updPay = payRepo.findByEmployeeID(updUser.getId());
+		updPay.setStartDate(pay.getStartDate());
+		updPay.setEndDate(pay.getEndDate());
+		updPay.setSickDays(pay.getSickDays());
+		updPay.setHolidays(pay.getHolidays());
+
+		payRepo.save(updPay);
+
+		model.addAttribute("message", "Updated Employee ID " + updUser.getId());
+
+		return admin;
+	}
 }
--- src/main/webapp/employee_mavericks.jsp
+++ src/main/webapp/employee_mavericks.jsp
@@ -2,6 +2,7 @@
 	pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,7 +34,7 @@
 			<table class="table table-striped table-bordered">
 				<tr>
 					<td>Pay Period</td>
-					<td>${pay.startDate} - ${pay.endDate}</td>
+					<td><input type="date" name="startDate" value="<fmt:formatDate value="${pay.startDate}" pattern="yyyy-MM-dd" />"/> - <input type="date" name="endDate" value="<fmt:formatDate value="${pay.endDate}" pattern="yyyy-MM-dd" />"/></td>
 				</tr>
 				<tr>
 					<td>Job Title</td>
